386edfa915b566315a9b93f81f62a40bbe074a6c
ditya5351-art <ditya5351@gmail.com>
2026-06-20 19:43:41 +0530

    Update db.py

--- db.py
+++ db.py
@@ -12,6 +12,7 @@ def get_connection():
     return mysql.connector.connect(
         host=st.secrets["mysql"]["host"],
         user=st.secrets["mysql"]["user"],
+        port=int(st.secrets["mysql"]["port"]),
         password=st.secrets["mysql"]["password"],
         database=st.secrets["mysql"]["database"],
         autocommit=False,
